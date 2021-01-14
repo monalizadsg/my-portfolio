9223c004372e4e63ef4b9f9d3c18ef1eae0d8944
Monaliza Dasig <monalizadasig@gmail.com>
2021-01-14 15:20:37 +0800

    Add technologies used in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Typed from "react-typed";
-import { Typography } from "@material-ui/core";
-import hero from "../images/hero.png";
+import { Typography, Grid } from "@material-ui/core";
 import "./Home.scss";
 
 const Home = () => {
@@ -27,42 +26,123 @@ const Home = () => {
           </Typography>
         </div>
         <div className='image-wrapper'>
-          <img src={hero} alt='hero art' className='image' />
+          <img
+            src={"/assets/images/hero.png"}
+            alt='hero art'
+            className='image'
+          />
         </div>
       </div>
-      <div className='about'>
-        <h2>About</h2>
-        <p>
-          I'm a self-taught developer. I started learning HTML, CSS and
-          Javascript. And now, I'm currently learning React. I'm very passionate
-          about learning and coding has been playing a huge part of my daily
-          life for the past few months. I'm self-driven person who enjoys
-          building apps and websites, and I quickly learn by doing which took me
-          out of my comfort zone.
-        </p>
-        <p>
-          I'm fascinated with technology as there is so much to learn new things
-          every day. Along my learning journey, I was able to train myself
-          consistently and pushing my own limits to discover ways how to solve a
-          problem. I firmly believe that practice is the key to become better at
-          something. I always enjoy learning new things and coding keeps me
-          uncomfortable but it is the only way to learn and grow.
-        </p>
-      </div>
-      <div className='contact'>
-        <h2>Contact Me</h2>
-        <p>Get in touch with me through these contacts:</p>
-        <div className='contact-details'>
-          <div className='detail'>
-            <h3>Contact number</h3>
-            <p>+63 9277912637</p>
-          </div>
-          <div className='detail'>
-            <h3>Email Address</h3>
-            <p>monalizadasig@gmail.com</p>
-          </div>
-        </div>
-      </div>
+      <main>
+        <article className='about'>
+          <section className='card'>
+            <div>
+              <h2 className='heading'>About</h2>
+            </div>
+            <p>
+              I'm a self-taught developer. I started learning HTML, CSS and
+              Javascript. And now, I'm currently learning React. I'm very
+              passionate about learning and coding has been playing a huge part
+              of my daily life for the past few months. I'm self-driven person
+              who enjoys building apps and websites, and I quickly learn by
+              doing which took me out of my comfort zone.
+            </p>
+            <p>
+              I'm fascinated with technology as there is so much to learn new
+              things every day. Along my learning journey, I was able to train
+              myself consistently and pushing my own limits to discover ways how
+              to solve a problem. I firmly believe that practice is the key to
+              become better at something. I always enjoy learning new things and
+              coding keeps me uncomfortable but it is the only way to learn and
+              grow.
+            </p>
+
+            <div>
+              <p>Here's a few technologies I've been working with recently:</p>
+              <div className='skill-container'>
+                <Grid container spacing={2} className='grid'>
+                  <Grid item xs={1}>
+                    {/* <Paper className={classes.paper}> */}
+                    <div className='skill-card'>
+                      <div className='skill-title'>HTML5</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/html.png"} alt='icon' />
+                      </div>
+                    </div>
+                    {/* </Paper> */}
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>CSS3</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/css.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>JavaScript</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/javascript.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>React</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/reactjs.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>Sass</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/sass.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>Material UI</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/materialui.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div className='skill-card'>
+                      <div className='skill-title'>Firebase</div>
+                      <div className='skill-logo'>
+                        <img src={"/assets/images/firebase.png"} alt='icon' />
+                      </div>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </section>
+        </article>
+        <article className='contact'>
+          <section className='card'>
+            <div>
+              <h2 className='heading'>Contact Me</h2>
+            </div>
+            <p>Get in touch with me through these contacts:</p>
+            <div className='contact-details'>
+              <div className='detail'>
+                <div>Contact number</div>
+                <p>+63 9277912637</p>
+              </div>
+              <div className='detail'>
+                <div>Email Address</div>
+                <p>monalizadasig@gmail.com</p>
+              </div>
+            </div>
+          </section>
+        </article>
+      </main>
     </div>
   );
 };
